cca7e73187182c1844091dbfcaf6ad1738c55f91
kawai10 <77530278+kawai10@users.noreply.github.com>
2021-03-27 20:54:11 +0900

    Create Program.java

---
+++ untitled/src/Program.java
@@ -0,0 +1,42 @@
+public class Program {
+    public static void main(String[] args) {
+        int[][] lottos = null;
+        int menu;
+        boolean running = true;
+
+
+        while (running)
+        {
+            menu = inputMenu();
+
+            switch (menu) {
+                case 1:
+                    lottos = createLottosAuto();
+                    break;
+                case 2:
+                    lottos = createLottosMenual();
+                    break;
+                case 3:
+                    printLottos(lottos);
+                    break;
+                case 4:
+                    running = false;
+                    break;
+                default:
+            }
+        }
+    }
+
+    private static void printLottos(int[][] lottos) {
+    }
+
+    private static int[][] createLottosMenual() {
+    }
+
+    private static int[][] createLottosAuto() {
+    }
+
+    private static int inputMenu() {
+        return 0;
+    }
+}
